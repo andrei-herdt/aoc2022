df1e994a842982172502b33af151857616fe48b0
Andrei Herdt <herdt@LP-L0247.local>
2022-12-17 15:46:26 +0000

    day1

--- day1.rs
+++ day1.rs
@@ -2,10 +2,13 @@ use std::fs;
 
 fn main() {
     let file_path = "input1.txt";
-    println!("In file {}", file_path);
 
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
+
+    for line in contents.chars(){
+        println!("{}", line.to_digit(10).to_string());
+    }
 
     println!("With text:\n{contents}");
 }
