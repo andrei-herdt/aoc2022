4c1aa681f373fd85d1c550901f73351877ca3cae
Andrei Herdt <herdt@LP-L0247.local>
2022-12-28 23:40:45 +0000

    update

--- day1.rs
+++ day1.rs
@@ -6,12 +6,18 @@ fn main() {
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
 
-    let _v = contents
-        .split("\n\n")
-        .inspect(|x| println!("{x}"))
-        .collect::<String>();
+    let _v: Vec<&str> = contents
+        .split_terminator("\n\n")
+        .collect();
 
     // Try map here
-    let _max = _v.parse::<u32>().into_iter().reduce(|acc, x| acc + x).unwrap();
-    println!("{}", _max);
+    for calories in _v {
+        let split_calories = calories.split_terminator("\n");
+        for calory in split_calories {
+            let parsed: u32 = calory.parse().unwrap();
+        }
+    }
+    // let _max = _v
+    //     .iter_into().split("\n").parse::<u32>().reduce(|acc, x| acc + x);
+    // println!("{}", _max);
 }
